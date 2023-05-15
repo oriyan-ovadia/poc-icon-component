a3f47ceba3f981e54cab44e320268c365a8fce11
Oriyan Ovadia <oriyan.ovadia@lemonade.com>
2023-05-15 16:09:31 +0300

    Update main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -35,41 +35,6 @@ module.exports = {
       ]
     });
 
-    // config.module.rules.push({
-    //   test: /\.svg$/,
-    //   use: [
-    //     '@svgr/webpack',
-    //     {
-    //       loader: 'svgo-loader',
-    //       options: {
-    //         icon: true,
-    //         js2svg: {
-    //           indent: 2,
-    //           pretty: true
-    //         },
-    //         multipass: true,
-    //         plugins: [
-    //           {
-    //             name: 'preset-default',
-    //             params: {
-    //               overrides: {
-    //                 /**
-    //                  * viewBox is required to resize SVGs with CSS.
-    //                  *
-    //                  * @see
-    //                  * - https://github.com/svg/svgo#svg-wont-scale-when-css-is-applied-on-it
-    //                  * - https://github.com/svg/svgo/issues/1128
-    //                  */
-    //                 removeViewBox: false
-    //               }
-    //             }
-    //           }
-    //         ]
-    //       }
-    //     }
-    //   ]
-    // });
-
     return config;
   }
 };
